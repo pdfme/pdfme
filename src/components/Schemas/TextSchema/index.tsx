cf2b3f70b44ad89cba28e98eec2b29ae8cfee377
hand-dot <kyouhei.fukuda0729@gmail.com>
2022-01-04 22:27:22 +0900

    minor fix

--- src/components/Schemas/TextSchema/index.tsx
+++ src/components/Schemas/TextSchema/index.tsx
@@ -3,36 +3,52 @@ import * as styles from './index.module.scss';
 import { zoom } from '../../../libs/constants';
 import { SchemaUIProps } from '../';
 
-// TODO レタースペーシングでPDFとズレる
 const TextSchema = (
   { schema, editable, placeholder, tabIndex, onChange }: SchemaUIProps,
   ref: Ref<HTMLTextAreaElement>
-) => (
-  <textarea
-    ref={ref}
-    disabled={!editable}
-    placeholder={placeholder}
-    tabIndex={tabIndex}
-    className={`${styles.placeholderGray} `}
-    style={{
-      resize: 'none',
-      fontFamily: schema.fontName || 'inherit',
-      height: schema.height * zoom,
-      width: (schema.width + (schema.characterSpacing || 0) * 0.75) * zoom, // 横幅を伸ばす1ポイントは0.75ピクセル
-      textAlign: schema.alignment,
-      fontSize: `${schema.fontSize}pt`,
-      letterSpacing: `${schema.characterSpacing}pt`,
-      fontFeatureSettings: `"palt"`,
-      lineHeight: `${schema.lineHeight}em`,
-      whiteSpace: 'pre-line',
-      wordBreak: 'break-all',
-      background: 'transparent',
-      border: 'none',
-      color: schema.fontColor || '#000',
-    }}
-    onChange={(e) => onChange(e.target.value)}
-    value={schema.data}
-  ></textarea>
-);
+) => {
+  const style: React.CSSProperties = {
+    resize: 'none',
+    fontFamily: schema.fontName || 'inherit',
+    height: schema.height * zoom,
+    width: schema.width * zoom,
+    textAlign: schema.alignment,
+    fontSize: `${schema.fontSize}pt`,
+    letterSpacing: `${schema.characterSpacing}pt`,
+    fontFeatureSettings: `"palt"`,
+    lineHeight: `${schema.lineHeight}em`,
+    whiteSpace: 'pre-line',
+    wordBreak: 'break-all',
+    background: 'transparent',
+    border: 'none',
+    color: schema.fontColor || '#000',
+  };
+
+  return editable ? (
+    <textarea
+      ref={ref}
+      placeholder={placeholder}
+      tabIndex={tabIndex}
+      className={`${styles.placeholderGray} `}
+      style={style}
+      onChange={(e) => onChange(e.target.value)}
+      value={schema.data}
+    ></textarea>
+  ) : (
+    <div style={style}>
+      {/*  Set the letterSpacing of the last character to 0. */}
+      {schema.data.split('').map((l, i) => (
+        <span
+          key={i}
+          style={{
+            letterSpacing: String(schema.data).length === i + 1 ? 0 : 'inherit',
+          }}
+        >
+          {l}
+        </span>
+      ))}
+    </div>
+  );
+};
 
 export default forwardRef<HTMLTextAreaElement, SchemaUIProps>(TextSchema);
