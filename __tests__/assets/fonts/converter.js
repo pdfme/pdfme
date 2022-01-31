1ccb2e11b0b2dad962d447f362c6bc26386a3165
hand-dot <kyouhei.fukuda0729@gmail.com>
2022-01-31 13:43:27 +0900

    remove unused file

--- __tests__/assets/fonts/converter.js
+++
@@ -1,6 +0,0 @@
-const fs = require("fs");
-
-const tgt = __dirname + "/" + process.argv[2];
-const ttf = fs.readFileSync(tgt + ".ttf");
-const base64 = Buffer.from(ttf).toString("base64");
-fs.writeFileSync(tgt + ".ts", `export default "${base64}";`);
