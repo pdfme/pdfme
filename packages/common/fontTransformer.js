b51d0e5112d47f9dab85d550ceff2b24c1803040
hand-dot <kyouhei.fukuda0729@gmail.com>
2022-02-01 14:00:38 +0900

    minor fix

--- packages/common/fontTransformer.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-const font = fs.readFileSync(path.join(__dirname, `src/assets/Helvetica.ttf`), {
-  encoding: 'base64',
-});
-
-module.exports = font;
--- packages/ui/assetsTransformer.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  process(src, filename, config, options) {
-    return 'module.exports = ' + JSON.stringify(path.basename(filename)) + ';';
-  },
-};
