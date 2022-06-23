84303804807ca8efea07c6538bbdf4c8fd7875cc
hand-dot <kyouhei.fukuda0729@gmail.com>
2022-06-23 16:43:09 +0900

    bugfix website build

--- website/src/components/DemoAppGridForm.tsx
+++ website/src/components/DemoAppGridForm.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import { HotTable } from '@handsontable/react';
-import { registerAllModules } from 'handsontable/registry';
-
-registerAllModules();
+import BrowserOnly from '@docusaurus/BrowserOnly';
 
 const DemoAppGridForm = ({
   datas,
@@ -12,26 +9,35 @@ const DemoAppGridForm = ({
   setDatas: (value: { [key: string]: string }[]) => void;
 }) => {
   return (
-    <HotTable
-      settings={{
-        afterChange: (changes) => {
-          if (!changes) return;
-          for (let i = 0; i < changes.length; i++) {
-            const change = changes[i];
-            const [row, column, _, nextValue] = change;
-            datas[row][column] = nextValue ?? '';
-          }
-          setDatas(datas);
-        },
-        colHeaders: datas[0] ? Object.keys(datas[0]) : [],
-        data: datas,
-        rowHeaders: true,
-        stretchH: 'all',
-        width: '100%',
-        height: 500,
-        licenseKey: 'non-commercial-and-evaluation',
+    <BrowserOnly fallback={<div>Loading...</div>}>
+      {() => {
+        const HotTable = require('@handsontable/react').HotTable;
+        const registerAllModules = require('handsontable/registry').registerAllModules;
+        registerAllModules();
+        return (
+          <HotTable
+            settings={{
+              afterChange: (changes) => {
+                if (!changes) return;
+                for (let i = 0; i < changes.length; i++) {
+                  const change = changes[i];
+                  const [row, column, _, nextValue] = change;
+                  datas[row][column] = nextValue ?? '';
+                }
+                setDatas(datas);
+              },
+              colHeaders: datas[0] ? Object.keys(datas[0]) : [],
+              data: datas,
+              rowHeaders: true,
+              stretchH: 'all',
+              width: '100%',
+              height: 500,
+              licenseKey: 'non-commercial-and-evaluation',
+            }}
+          />
+        );
       }}
-    />
+    </BrowserOnly>
   );
 };
 
