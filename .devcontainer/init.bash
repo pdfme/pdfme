1d0c6eef454eb9d9b3d946bb90b6a6235c3a136c
Kyohei Fukuda <kyouhei.fukuda0729@gmail.com>
2023-08-15 14:54:23 +0000

    change

--- .devcontainer/init.bash
+++ .devcontainer/init.bash
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+set -eu
+cd $(dirname $0)
+
+cd ../
+sed -i -e 's#git@github.com:#https://github.com/#g' .gitmodules
+sed -i -e 's#git@github.com:#https://github.com/#g' .git/config
+git submodule update --init --recursive
 cd playground/
 npm i
 npm run start
