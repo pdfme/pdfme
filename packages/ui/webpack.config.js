45d18a5f1f81e3c6b65115247d65ce2a51d8b974
Kyohei Fukuda <kyouhei.fukuda0729@gmail.com>
2023-11-07 10:52:55 +0900

    npm run build command to parallel

--- packages/ui/webpack.config.js
+++ packages/ui/webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const webpack = require('webpack');
 const pkg = require('./package.json');
+const TerserPlugin = require('terser-webpack-plugin');
+// const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 const isProduction = process.env.NODE_ENV === 'production';
 
@@ -12,7 +14,16 @@ const BANNER = [
 ].join('\n');
 
 const config = {
-  optimization: { minimize: isProduction },
+  mode: isProduction ? 'production' : 'development',
+  optimization: {
+    minimize: isProduction,
+    minimizer: isProduction ? [new TerserPlugin({
+      parallel: true,
+      terserOptions: {
+
+      },
+    })] : [],
+  },
   resolve: {
     extensions: ['.ts', '.tsx', '.js'],
     alias: { process: 'process/browser' },
@@ -22,7 +33,7 @@ const config = {
     new webpack.ProvidePlugin({ process: 'process/browser' }),
     new webpack.BannerPlugin({ banner: BANNER, entryOnly: true }),
   ],
-  devtool: 'source-map',
+  devtool: isProduction ? false : 'eval-cheap-module-source-map',
   entry: './src/index.ts',
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -32,7 +43,7 @@ const config = {
   },
   module: {
     rules: [
-      { test: /\.tsx?$/, use: 'ts-loader' },
+      { test: /\.tsx?$/, use: 'ts-loader', exclude: /node_modules/ },
       { test: /\.css$/i, use: ['style-loader', 'css-loader'] }
     ]
   },
