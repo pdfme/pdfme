aeb446feb43c4f2019f138c930f27f84c46d3606
Pete Ward <pete@pennyblack.io>
2023-12-04 15:33:08 +0000

    remove React.StrictMode in playground to avoid full rerender on screen resize

--- playground/src/index.tsx
+++ playground/src/index.tsx
@@ -10,12 +10,10 @@ import Navigation from "./Navigation";
 const container = document.getElementById('root')!;
 const root = createRoot(container);
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Navigation />
-      <Routes>
-        <Route path="/" element={<Designer />}></Route>
-        <Route path="/form-viewer" element={<FormAndViewer />}></Route>
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>);
+  <BrowserRouter>
+    <Navigation />
+    <Routes>
+      <Route path="/" element={<Designer />}></Route>
+      <Route path="/form-viewer" element={<FormAndViewer />}></Route>
+    </Routes>
+  </BrowserRouter>);
