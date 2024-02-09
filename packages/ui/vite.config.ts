7f972924d0525bd1baba3f45dec0bc8ef9f2985e
Olga Krayevska <olga.krayevska435@gmail.com>
2024-02-09 16:48:34 +0200

    ui-build-optimization: process.env.NODE_ENV defined in vite.configs.ts, only prod react/react-dom files are included in build

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -3,17 +3,20 @@ import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 
-export default defineConfig({
-  plugins: [react(), tsconfigPaths({ root: '.' }), cssInjectedByJsPlugin()],
-  build: {
-    lib: {
-      entry: 'src/index.ts',
-      name: '@pdfme/ui',
-      fileName: (format) => `index.${format}.js`,
+export default defineConfig(({ mode }) => {
+  return {
+    define: { 'process.env.NODE_ENV': JSON.stringify(mode) },
+    plugins: [react(), tsconfigPaths({ root: '.' }), cssInjectedByJsPlugin()],
+    build: {
+      lib: {
+        entry: 'src/index.ts',
+        name: '@pdfme/ui',
+        fileName: (format) => `index.${format}.js`,
+      },
     },
-  },
-  optimizeDeps: {
-    include: ['react', 'react-dom', 'pdfjs-dist', 'antd'],
-    exclude: ['@pdfme/common', '@pdfme/schemas'],
-  },
+    optimizeDeps: {
+      include: ['react', 'react-dom', 'pdfjs-dist', 'antd'],
+      exclude: ['@pdfme/common', '@pdfme/schemas'],
+    },
+  };
 });
