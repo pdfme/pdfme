0fc48f60846797804c19082e466e2ed3cebfe32b
Pete Ward <pete@pennyblack.io>
2024-08-07 16:08:15 +0100

    f

--- packages/schemas/src/multiVariableText/helper.ts
+++ packages/schemas/src/multiVariableText/helper.ts
@@ -1,21 +1,23 @@
 export const substituteVariables = (text: string, variablesIn: string | Record<string, string>): string => {
-  if (!text || !variablesIn) {
-    return text;
+  if (!text) {
+    return "";
   }
 
   let substitutedText = text;
 
-  const variables: Record<string, string> = (typeof variablesIn === "string") ? JSON.parse(variablesIn) || {} : variablesIn;
+  if (variablesIn) {
+    const variables: Record<string, string> = (typeof variablesIn === "string") ? JSON.parse(variablesIn) || {} : variablesIn;
 
-  Object.keys(variables).forEach((variableName) => {
-    // handle special characters in variable name
-    const variableForRegex = variableName.replace(/[/\-\\^$*+?.()|[\]{}]/g, '\\$&');
-    const regex = new RegExp('{' + variableForRegex + '}', 'g');
-    substitutedText = substitutedText.replace(regex, variables[variableName]);
-  });
+    Object.keys(variables).forEach((variableName) => {
+      // handle special characters in variable name
+      const variableForRegex = variableName.replace(/[/\-\\^$*+?.()|[\]{}]/g, '\\$&');
+      const regex = new RegExp('{' + variableForRegex + '}', 'g');
+      substitutedText = substitutedText.replace(regex, variables[variableName]);
+    });
+  }
 
   // Remove any variables that were not substituted from inputs
-  substitutedText.replace(/{[^{}]+}/g, '');
+  substitutedText = substitutedText.replace(/{[^{}]+}/g, '');
 
   return substitutedText;
 };
