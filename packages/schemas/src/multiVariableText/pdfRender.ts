90679f27cf0c8b58ad2394321c2fafde5e15fae2
Peter Ward <pete@pennyblack.io>
2024-09-05 02:50:44 +0100

    validate MVT field contents and skip rendering when non-required variables are missing a value (#585)

--- packages/schemas/src/multiVariableText/pdfRender.ts
+++ packages/schemas/src/multiVariableText/pdfRender.ts
@@ -1,13 +1,19 @@
 import { PDFRenderProps } from '@pdfme/common';
 import { MultiVariableTextSchema } from './types';
 import { pdfRender as parentPdfRender } from '../text/pdfRender';
-import { substituteVariables } from './helper';
+import { substituteVariables, validateVariables } from './helper';
 
 export const pdfRender = async (arg: PDFRenderProps<MultiVariableTextSchema>) => {
-  const { value, schema, ...rest } = arg;
+  const { key, value, schema, ...rest } = arg;
+
+  if (!validateVariables(key, value, schema)) {
+    // Don't render if a required variable is missing
+    return;
+  }
 
   const renderArgs = {
-    value:  substituteVariables(schema.text || '', value),
+    key,
+    value: substituteVariables(schema.text || '', value),
     schema,
     ...rest,
   };
