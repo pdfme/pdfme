8818a29a0185d049589ee1bd708435d31f88f481
Kyohei Fukuda <kyoheif@wix.com>
2025-01-22 20:54:15 +0900

    TMP

--- packages/schemas/src/graphics/pdfHelper.ts
+++ packages/schemas/src/graphics/pdfHelper.ts
@@ -1,4 +1,5 @@
-import { UIRenderProps } from '@pdfme/common';
+import { UIRenderProps, b64toUint8Array } from '@pdfme/common';
+import { pdf2img } from '@pdfme/converter';
 import { getCacheKey } from './cacheKey';
 import { ImageSchema } from './image';
 
@@ -14,44 +15,18 @@ export const pdfToImage = async ({
   schema,
   value,
   _cache,
-}: // TODO pdfJsは削除されているので、代替案を検討する
-// pdfJs,
-UIRenderProps<ImageSchema>): Promise<string> => {
+}: UIRenderProps<ImageSchema>): Promise<string> => {
   // using value from cache to prevent rerending pdf to image
-  // const pdfImageCacheKey = getCacheKey(schema, value);
-  // const imageSrc = _cache.get(pdfImageCacheKey);
-  // if (imageSrc) return imageSrc;
+  const pdfImageCacheKey = getCacheKey(schema, value);
+  const imageSrc = _cache.get(pdfImageCacheKey);
+  if (imageSrc) return imageSrc;
 
-  // const pdfDoc = await pdfJs.getDocument({ url: value }).promise;
-  // const page = await pdfDoc.getPage(1);
-
-  // const canvas = document.createElement('canvas');
-  // const viewport = page.getViewport({ scale: 1 });
-  // canvas.width = viewport.width;
-  // canvas.height = viewport.height;
-  // const canvasContext = canvas.getContext('2d')!;
-
-  // await page.render({ canvasContext, viewport }).promise;
-  // const image = await _canvasToObjectUrl(canvas);
-
-  // _cache.set(pdfImageCacheKey, image);
-  // return image;
-  return '';
-};
-
-/**
- * convert canvas to blob url
- * @param canvas
- */
-const _canvasToObjectUrl = async (canvas: HTMLCanvasElement): Promise<string> => {
-  return new Promise((resolve, reject) => {
-    canvas.toBlob(function (blob) {
-      if (!blob) {
-        reject('inavlid canvas');
-      } else {
-        const url = URL.createObjectURL(blob);
-        resolve(url);
-      }
-    });
+  const images = await pdf2img(b64toUint8Array(value), {
+    imageType: 'png',
+    range: { end: 1 },
   });
+  const image = URL.createObjectURL(new Blob([images[0]], { type: 'image/png' }));
+
+  _cache.set(pdfImageCacheKey, image);
+  return image;
 };
