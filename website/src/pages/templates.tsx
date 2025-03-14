a4175f89faeee982477e265bd78a1af86439f388
Kyohei Fukuda <kyoheif@wix.com>
2025-03-14 10:03:05 +0900

    Enhance template navigation by adding locale support in TemplateDesign component

--- website/src/pages/templates.tsx
+++ website/src/pages/templates.tsx
@@ -1,17 +1,21 @@
 import React, { useEffect } from 'react';
 import Layout from '@theme/Layout';
 import { useHistory } from '@docusaurus/router';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import { playgroundUrl } from '../constants';
 
 const TemplateDesign = () => {
   const history = useHistory();
+  const { i18n } = useDocusaurusContext();
+  const { currentLocale } = i18n;
 
   useEffect(() => {
     const handleMessage = (e: MessageEvent) => {
       if (e.data?.type === 'navigate') {
         const { name = "", ui = "" } = e.data?.payload;
         if (name && ui) {
-          history.push(`/template-design?template=${name}&ui=${ui}`);
+            const localePath = currentLocale === 'en' ? '' : `/${currentLocale}`;
+            history.push(`${localePath}/template-design?template=${name}&ui=${ui}`);
         }
       }
     };
@@ -20,7 +24,7 @@ const TemplateDesign = () => {
     return () => {
       window.removeEventListener('message', handleMessage);
     };
-  }, [history]);
+  }, [history, currentLocale]);
 
   return (
     <Layout title="Sample Templates" description="Explore sample templates with our interactive playground.">
