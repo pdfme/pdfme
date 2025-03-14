607356e554320c2c1980aadf7b9f8c72e5f80e65
hitomi-t260g <s_t_u_d_i_a@yahoo.co.jp>
2025-03-14 11:41:15 +0900

    restore NotoSerifJP and NotoSansJP Font setting for playground

--- playground/src/helper.ts
+++ playground/src/helper.ts
@@ -12,14 +12,14 @@ export function fromKebabCase(str: string): string {
 
 export const getFontsData = (): Font => ({
   ...getDefaultFont(),
-  // NotoSerifJP: {
-  //   fallback: false,
-  //   data: 'https://fonts.gstatic.com/s/notoserifjp/v30/xn71YHs72GKoTvER4Gn3b5eMRtWGkp6o7MjQ2bwxOubAILO5wBCU.ttf',
-  // },
-  // NotoSansJP: {
-  //   fallback: false,
-  //   data: 'https://fonts.gstatic.com/s/notosansjp/v53/-F6jfjtqLzI2JPCgQBnw7HFyzSD-AsregP8VFBEj75vY0rw-oME.ttf',
-  // },
+  NotoSerifJP: {
+    fallback: false,
+    data: 'https://fonts.gstatic.com/s/notoserifjp/v30/xn71YHs72GKoTvER4Gn3b5eMRtWGkp6o7MjQ2bwxOubAILO5wBCU.ttf',
+  },
+  NotoSansJP: {
+    fallback: false,
+    data: 'https://fonts.gstatic.com/s/notosansjp/v53/-F6jfjtqLzI2JPCgQBnw7HFyzSD-AsregP8VFBEj75vY0rw-oME.ttf',
+  },
   'PinyonScript-Regular': {
     fallback: false,
     data: 'https://fonts.gstatic.com/s/pinyonscript/v22/6xKpdSJbL9-e9LuoeQiDRQR8aOLQO4bhiDY.ttf',
