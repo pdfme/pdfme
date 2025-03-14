a9cca762f8aa8e354d5569f1db3cb1163dc909b9
hand-dot <kyouhei.fukuda0729@gmail.com>
2025-03-14 21:19:06 +0900

    feat: add PinyonScript font to fonts data in helper

--- playground/src/helper.ts
+++ playground/src/helper.ts
@@ -12,6 +12,10 @@ export function fromKebabCase(str: string): string {
 
 export const getFontsData = (): Font => ({
   ...getDefaultFont(),
+  'PinyonScript-Regular': {
+    fallback: false,
+    data: 'https://fonts.gstatic.com/s/pinyonscript/v22/6xKpdSJbL9-e9LuoeQiDRQR8aOLQO4bhiDY.ttf',
+  },
   NotoSerifJP: {
     fallback: false,
     data: 'https://fonts.gstatic.com/s/notoserifjp/v30/xn71YHs72GKoTvER4Gn3b5eMRtWGkp6o7MjQ2bwxOubAILO5wBCU.ttf',
@@ -19,11 +23,7 @@ export const getFontsData = (): Font => ({
   NotoSansJP: {
     fallback: false,
     data: 'https://fonts.gstatic.com/s/notosansjp/v53/-F6jfjtqLzI2JPCgQBnw7HFyzSD-AsregP8VFBEj75vY0rw-oME.ttf',
-  },
-  'PinyonScript-Regular': {
-    fallback: false,
-    data: 'https://fonts.gstatic.com/s/pinyonscript/v22/6xKpdSJbL9-e9LuoeQiDRQR8aOLQO4bhiDY.ttf',
-  },
+  }
 });
 
 export const readFile = (file: File | null, type: 'text' | 'dataURL' | 'arrayBuffer') => {
