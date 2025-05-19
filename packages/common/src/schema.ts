1a5fdfc00de3c29b3e0937d6430338a4d180cbe3
Tyler Barker <hey@tylerbarker.com>
2025-05-19 17:19:09 +1000

    Extend UIOptions schema

--- packages/common/src/schema.ts
+++ packages/common/src/schema.ts
@@ -202,6 +202,9 @@ export const UIOptions = CommonOptions.extend({
   icons: z.record(z.string(), z.string()).optional(),
   requiredByDefault: z.boolean().optional(),
   maxZoom: z.number().optional(),
+  pageCursor: z.number().optional(),
+  sidebarOpen: z.boolean().optional(),
+  zoomLevel: z.number().optional(),
 });
 
 const HTMLElementSchema: z.ZodSchema<HTMLElement> = z.any().refine((v) => v instanceof HTMLElement);
