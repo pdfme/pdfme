03fccd3109047683879cca3645de84a366119aee
hand-dot <kyouhei.fukuda0729@gmail.com>
2026-06-09 18:45:01 +0900

    feat(designer): increase max zoom level to enhance design flexibility fix(manifest): update field count for template metadata

--- playground/src/routes/Designer.tsx
+++ playground/src/routes/Designer.tsx
@@ -546,7 +546,7 @@ function DesignerApp() {
               multiVariableText:
                 '<svg fill="#000000" width="24px" height="24px" viewBox="0 0 24 24"><path d="M6.643,13.072,17.414,2.3a1.027,1.027,0,0,1,1.452,0L20.7,4.134a1.027,1.027,0,0,1,0,1.452L9.928,16.357,5,18ZM21,20H3a1,1,0,0,0,0,2H21a1,1,0,0,0,0-2Z"/></svg>',
             },
-            maxZoom: 250,
+            maxZoom: 500,
           },
           plugins: getPlugins(),
         });
